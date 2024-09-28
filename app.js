287035112cacf2ded1155a961dbfea82b2b78981
qm <qiuming@oray.com>
2024-09-28 23:23:27 +0800

    reconstruct settings.view

--- app.js
+++ app.js
@@ -653,122 +653,123 @@ window.init_about = function()
 function init_setting_( extension )
 {
 
-  var tabactive = Q.$(location.hash.replace( /^#/g, "" ) );
-  if( !tabactive ) {
-    tabactive = Q.$('tab-basic');
-  }
+ // var tabactive = Q.$(location.hash.replace( /^#/g, "" ) );
+  //if( !tabactive ) {
+    //tabactive = Q.$('tab-basic');
+  //}
   
-  var t2 = new Q.tabs({
-    action: "click",
-    active: tabactive,
-    onactive: function( tid ) {
-      var stateObject = {};
-      var newUrl = "#" + tid.id;
-      history.pushState(stateObject,tid.innerText,newUrl);
-    },
-    items: [
-      {tab: Q.$('tab-basic'), content: Q.$('panel-1-1')},
-      {tab: Q.$('tab-download'), content: Q.$('panel-1-2')},
-      {tab: Q.$('tab-screencapture'), content: Q.$('panel-1-3')},
-      {tab: Q.$('tab-shortcut'), content: Q.$('panel-1-4')},
-    ]  
-  });
+  //var t2 = new Q.tabs({
+    //action: "click",
+    //active: tabactive,
+    //onactive: function( tid ) {
+      //var stateObject = {};
+      //var newUrl = "#" + tid.id;
+      //history.pushState(stateObject,tid.innerText,newUrl);
+    //},
+    //items: [
+      //{tab: Q.$('tab-basic'), content: Q.$('panel-1-1')},
+      //{tab: Q.$('tab-download'), content: Q.$('panel-1-2')},
+      //{tab: Q.$('tab-screencapture'), content: Q.$('panel-1-3')},
+      //{tab: Q.$('tab-shortcut'), content: Q.$('panel-1-4')},
+    //]  
+  //});
 
-  var assistant_url = extension.wayixia_assistant;
-  if( assistant_url != "" ) {
-    Q.$('wayixia-assistant-ing').style.display = '';
-    Q.ajax( {
-      command: assistant_url + '/keepalive',
-      oncomplete: function(xml) {
-        if( xml.responseText == "connected") {
-          Q.$('wayixia-assistant-ing').style.display = 'none';
-          Q.$('wayixia-assistant-ok').style.display = '';
-          Q.$('wayixia-assistant-error').style.display = 'none';
-        } else{
-          Q.$('wayixia-assistant-ing').style.display = 'none';
-          Q.$('wayixia-assistant-ok').style.display = 'none';
-          Q.$('wayixia-assistant-error').style.display = '';
-        }
+  //var assistant_url = extension.wayixia_assistant;
+  //if( assistant_url != "" ) {
+    //Q.$('wayixia-assistant-ing').style.display = '';
+    //Q.ajax( {
+      //command: assistant_url + '/keepalive',
+      //oncomplete: function(xml) {
+        //if( xml.responseText == "connected") {
+          //Q.$('wayixia-assistant-ing').style.display = 'none';
+          //Q.$('wayixia-assistant-ok').style.display = '';
+          //Q.$('wayixia-assistant-error').style.display = 'none';
+        //} else{
+          //Q.$('wayixia-assistant-ing').style.display = 'none';
+          //Q.$('wayixia-assistant-ok').style.display = 'none';
+          //Q.$('wayixia-assistant-error').style.display = '';
+        //}
 
-      },
-      onerror: function(xml) {
-        Q.$('wayixia-assistant-ing').style.display = 'none';
-        Q.$('wayixia-assistant-ok').style.display = 'none';
-        Q.$('wayixia-assistant-error').style.display = '';
-      } 
-    } );
-  } else {
-    Q.$('wayixia-assistant-ing').style.display = 'none';
-    Q.$('wayixia-assistant-ok').style.display = 'none';
-    Q.$('wayixia-assistant-error').style.display = '';
-  }
+      //},
+      //onerror: function(xml) {
+        //Q.$('wayixia-assistant-ing').style.display = 'none';
+        //Q.$('wayixia-assistant-ok').style.display = 'none';
+        //Q.$('wayixia-assistant-error').style.display = '';
+      //} 
+    //} );
+  //} else {
+    //Q.$('wayixia-assistant-ing').style.display = 'none';
+    //Q.$('wayixia-assistant-ok').style.display = 'none';
+    //Q.$('wayixia-assistant-error').style.display = '';
+  //}
 
 
-  // save path
-  Q.$('save_path').value = extension.user_config_get('save_path');
-  new Q.PlaceHolder( {
-      holder: "label_save_path", 
-      id: "save_path", 
-  } );
+  //// save path
+  //Q.$('save_path').value = extension.save_path;
+  //new Q.PlaceHolder( {
+      //holder: "label_save_path", 
+      //id: "save_path", 
+  //} );
   
-  // date folder
-  var option_date_folder = (extension.user_config_get('date_folder') != '0');
-  var date_folder = new Q.CheckBox({ id : "date_folder", checked: option_date_folder});
+  //// date folder
+  //var option_date_folder = (extension.date_folder != '0');
+  //var date_folder = new Q.CheckBox({ id : "date_folder", checked: option_date_folder});
 
-  // Sitename folder
-  var save_lastconfig = new Q.CheckBox({ id : "save_lastconfig", checked: extension.save_lastconfig() });
+  //// Sitename folder
+  //var save_lastconfig = new Q.CheckBox({ id : "save_lastconfig", checked: extension.save_lastconfig() });
 
-  // filter rules
-  var option_filter_rules = extension.filter_rule_is_enabled();
-  Q.$('manager_filter_rules').disabled = !option_filter_rules;
-  var filter_rules = new Q.CheckBox({ id : "filter_rules_enable",
-    checked: option_filter_rules,
-    onchange : function(checked) {
-      Q.$('manager_filter_rules').disabled = !checked;
-    }
-  });
+  //// filter rules
+  //var option_filter_rules = extension.filter_rule_is_enabled();
+  //Q.$('manager_filter_rules').disabled = !option_filter_rules;
+  //var filter_rules = new Q.CheckBox({ id : "filter_rules_enable",
+    //checked: option_filter_rules,
+    //onchange : function(checked) {
+      //Q.$('manager_filter_rules').disabled = !checked;
+    //}
+  //});
 
-  // Account buttons
-  var wayixia_account_text = "" ;
-  if( extension.nickname() != "" ) {
-    wayixia_account_text = locale_text('stringAccountLogout') + " ( " + extension.nickname().toUpperCase() + " )";
-  } else {
-    wayixia_account_text = locale_text('stringAccountLogin');
-  }
-  Q.$('wayixia_account').innerHTML = wayixia_account_text;  
-  Q.$('wayixia_account').onclick = ( function( ext ) { return function() {
-    wayixia_track_button_click(this);
-    if( extension.nickname() != "" ) {
-      ext.wayixia_logout( function(ok) {
-        location.reload(); 
-      } );
-    } else {
-      // login
-      window.open("https://www.wayixia.com/?mod=user&action=login&refer="+location.href);
-    }
-  } } )( extension );
+  //// Account buttons
+  //var wayixia_account_text = "" ;
+  //if( extension.nickname() != "" ) {
+    //wayixia_account_text = locale_text('stringAccountLogout') + " ( " + extension.nickname().toUpperCase() + " )";
+  //} else {
+    //wayixia_account_text = locale_text('stringAccountLogin');
+  //}
+  //Q.$('wayixia_account').innerHTML = wayixia_account_text;  
+  //Q.$('wayixia_account').onclick = ( function( ext ) { return function() {
+    //wayixia_track_button_click(this);
+    //if( extension.nickname() != "" ) {
+      //ext.wayixia_logout( function(ok) {
+        //location.reload(); 
+      //} );
+    //} else {
+      //// login
+      //window.open("https://www.wayixia.com/?mod=user&action=login&refer="+location.href);
+    //}
+  //} } )( extension );
 
 
   g_option_window = require('./src/views/settings.view')({
     title: locale_text('extOptions')
   });
   
-  Q.$('layer-options').style.visibility = 'visible';
-  g_option_window.domodal($GetDesktopWindow());
+  //Q.$('layer-options').style.visibility = 'visible';
+  //g_option_window.domodal($GetDesktopWindow());
+  g_option_window.domodal();
 
-  // block images
-  Q.$('manager_block_images').onclick = function() {
-    wayixia_track_button_click(this);
-    g_block_images_box = new Q.ImagesBox({id: 'wayixia-list'});
-    var extension = chrome.extension.getBackgroundPage();
-    var block_images = extension.block_images_all();
-    g_block_images_box.display_images(block_images, {})();  
-    display_block_images();
-  }
+//  // block images
+  //Q.$('manager_block_images').onclick = function() {
+    //wayixia_track_button_click(this);
+    //g_block_images_box = new Q.ImagesBox({id: 'wayixia-list'});
+    //var extension = chrome.extension.getBackgroundPage();
+    //var block_images = extension.block_images_all();
+    //g_block_images_box.display_images(block_images, {})();  
+    //display_block_images();
+  //}
   
-  Q.$('manager_filter_rules').onclick = function() {
-    display_filter_rules();
-  }
+  //Q.$('manager_filter_rules').onclick = function() {
+    //display_filter_rules();
+  //}
 }
 
 
